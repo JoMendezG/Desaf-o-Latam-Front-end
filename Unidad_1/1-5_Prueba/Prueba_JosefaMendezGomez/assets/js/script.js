5a0aad5639c90a3fb475daa1731434288982eed3
Josefa <jmendezg@udd.cl>
2021-05-13 10:48:54 -0400

    Se incorporó el smooth del navbar

--- Unidad_1/1-5_Prueba/Prueba_JosefaMendezGomez/assets/js/script.js
+++ Unidad_1/1-5_Prueba/Prueba_JosefaMendezGomez/assets/js/script.js
@@ -1,1 +1,46 @@
+// Formulario
 (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()
+
+
+// smooth scroll
+// No olvidar colocar 
+// html {
+//     scroll-behavior: smooth;
+//   }
+//En el css
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  })
+
+  
+
+$(document).ready(function() {
+    // Alerta
+    $('#miAlerta').click(function() {
+        alert('El correo fue enviado correctamente')
+    })
+
+})
+
+
