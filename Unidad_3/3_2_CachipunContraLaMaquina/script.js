bb52d9f1092ca39172db144b5bac7f78e4643b52
Josefa <jmendezg@udd.cl>
2021-06-28 22:33:23 -0400

    html cachipun listo

---
+++ Unidad_3/3_2_CachipunContraLaMaquina/script.js
@@ -0,0 +1,7 @@
+// Crear usuario
+let txtUsuario = document.getElementById('usuario');
+txtUsuario.textContent = prompt('Ingresa nombre de usuario');
+
+// Crear oponente
+let txtOponente = document.getElementById('oponente');
+txtOponente.textContent = prompt('Ingresa nombre de oponente');
