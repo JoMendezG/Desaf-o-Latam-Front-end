863016a1c1539ddb5393c767e956af029a1739b4
Josefa <jmendezg@udd.cl>
2021-06-29 01:05:12 -0400

    Definición de ganar, perder o empatar listo

--- Unidad_3/3_2_CachipunContraLaMaquina/script.js
+++ Unidad_3/3_2_CachipunContraLaMaquina/script.js
@@ -6,20 +6,52 @@ txtUsuario.textContent = prompt('Ingresa nombre de usuario');
 let txtOponente = document.getElementById('oponente');
 txtOponente.textContent = prompt('Ingresa nombre de oponente');
 
-function papel() {
-    console.log("Eligiste papel");
-    document.getElementById("tuSelect").className = "far fa-hand-paper fa-5x";
+let tuJugada;
+let resultado = document.getElementById('resultado');
+
+
+// Posibilidades del oponente
+function jugadaOponente() {
+    // Condicional random de 3 opciones para oponente
+    let oponente = Math.floor(Math.random() * (3 - 1 + 1) + 1); 
     
+    if (oponente == 1) {
+        document.getElementById("selectOponente").className = "far fa-hand-paper fa-5x";
+    } else if (oponente == 2) {
+        document.getElementById("selectOponente").className = "far fa-hand-scissors fa-5x";
+    } else if (oponente == 3) {
+        document.getElementById("selectOponente").className = "far fa-hand-rock fa-5x";
+    }
+
+    ganar(oponente);
+}
+
+function papel() {
+    document.getElementById("tuSelect").className = "far fa-hand-paper fa-5x";
+    tuJugada = 1;
+    jugadaOponente(); 
 }
 
 function tijera() {
-    console.log("Eligiste tijera");
     document.getElementById("tuSelect").className = "far fa-hand-scissors fa-5x";
+    tuJugada = 2;
+    jugadaOponente(); 
 }
 
 function piedra() {
-    console.log("Eligiste piedra");
     document.getElementById("tuSelect").className = "far fa-hand-rock fa-5x";
-
+    tuJugada = 3;
+    jugadaOponente(); 
 }
 
+// Ver quien gana
+function ganar(opJugada) {
+    if (opJugada == 1 && tuJugada == 1 || opJugada == 2 && tuJugada == 2 || opJugada == 3 && tuJugada == 3) {
+        resultado.textContent = "Empate";
+        console.log("empate")
+    } else if (opJugada == 1 && tuJugada == 3 || opJugada == 2 && tuJugada == 1 || opJugada == 3 && tuJugada == 2) {
+        resultado.textContent = "Pierdes";
+    } else {
+        resultado.textContent = "Ganas";
+    }
+}
