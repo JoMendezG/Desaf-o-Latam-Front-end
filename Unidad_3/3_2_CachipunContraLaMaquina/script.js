685d3c3ba598e7ef807d6be8db0417ed6071ee76
Josefa <jmendezg@udd.cl>
2021-06-28 23:50:35 -0400

    Botones y mi eleccion listas

--- Unidad_3/3_2_CachipunContraLaMaquina/script.js
+++ Unidad_3/3_2_CachipunContraLaMaquina/script.js
@@ -5,3 +5,21 @@ txtUsuario.textContent = prompt('Ingresa nombre de usuario');
 // Crear oponente
 let txtOponente = document.getElementById('oponente');
 txtOponente.textContent = prompt('Ingresa nombre de oponente');
+
+function papel() {
+    console.log("Eligiste papel");
+    document.getElementById("tuSelect").className = "far fa-hand-paper fa-5x";
+    
+}
+
+function tijera() {
+    console.log("Eligiste tijera");
+    document.getElementById("tuSelect").className = "far fa-hand-scissors fa-5x";
+}
+
+function piedra() {
+    console.log("Eligiste piedra");
+    document.getElementById("tuSelect").className = "far fa-hand-rock fa-5x";
+
+}
+
